aa73fd63f13c3cba8601c24d018651adf97ba33f
MukundAgarawal <121181610+MukundAgarawal@users.noreply.github.com>
2025-06-12 12:28:17 +0530

    Create km_to_mile.java

---
+++ Java_Programming_Elements/Java_Programming_Elements/Level_1/km_to_mile.java
@@ -0,0 +1,14 @@
+package immersion;
+
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int c = sc.nextInt();
+//		int Kilometers = sc.nextInt();
+//		int b = sc.nextInt();
+		double km = 10.8;
+		System.out.println(10.8*1.6);
+	}
+}
