93a75ad3d17c3805019f66932b13ac3cb2e1642c
MukundAgarawal <121181610+MukundAgarawal@users.noreply.github.com>
2025-06-12 12:26:59 +0530

    Create sam_marks.java

---
+++ Java_Programming_Elements/Java_Programming_Elements/Level_1/sam_marks.java
@@ -0,0 +1,16 @@
+package immersion;
+
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int c = sc.nextInt();
+//		int Kilometers = sc.nextInt();
+//		int b = sc.nextInt();
+		int m = 94;
+		int p = 95;
+		int c = 96;
+		System.out.println((m+p+c)/3);
+	}
+}
