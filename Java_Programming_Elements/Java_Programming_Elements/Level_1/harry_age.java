1d51233db3ea1adedb96adc07be36b3dcdcd959c
MukundAgarawal <121181610+MukundAgarawal@users.noreply.github.com>
2025-06-12 12:25:05 +0530

    Update and rename Dummy.java to harry_age.java

---
+++ Java_Programming_Elements/Java_Programming_Elements/Level_1/harry_age.java
@@ -0,0 +1,15 @@
+package immersion;
+
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int c = sc.nextInt();
+//		int Kilometers = sc.nextInt();
+//		int b = sc.nextInt();
+		int birth = 2000;
+		int curr = 2024;
+		System.out.println(curr - birth);
+	}
+}
