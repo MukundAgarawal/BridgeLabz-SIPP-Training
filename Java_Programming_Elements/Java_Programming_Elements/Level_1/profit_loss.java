0e3bbdd42322e88204c04cbedf624cd5e8164aa3
MukundAgarawal <121181610+MukundAgarawal@users.noreply.github.com>
2025-06-12 12:30:33 +0530

    Create profit_loss.java

---
+++ Java_Programming_Elements/Java_Programming_Elements/Level_1/profit_loss.java
@@ -0,0 +1,16 @@
+package immersion;
+
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int c = sc.nextInt();
+//		int Kilometers = sc.nextInt();
+//		int b = sc.nextInt();
+		int cp = 129;
+		int sp = 191;
+		int p = sp-cp;
+		System.out.println("The Profit is INR " + (p) + " and the Profit Percentage is " + (p*100/cp));
+	}
+}
