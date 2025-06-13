a03e0dfb58fba310bd801b4396103b0b2922b87b
MukundAgarawal <121181610+MukundAgarawal@users.noreply.github.com>
2025-06-13 08:45:53 +0530

    Create para_to_side.java

---
+++ Java_Programming_Elements/Java_Programming_Elements/Level_1/para_to_side.java
@@ -0,0 +1,14 @@
+package immersion;
+
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int c = sc.nextInt();
+		double p = sc.nextInt();
+//		double b = sc.nextInt();
+//		int b = sc.nextInt();
+		System.out.println("The length of the side is " + (p/4) + " whose perimeter is " + (p));
+	}
+}
