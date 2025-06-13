2ce1d48e45b0749735be607278e6abd067424999
MukundAgarawal <121181610+MukundAgarawal@users.noreply.github.com>
2025-06-13 08:52:01 +0530

    Create handshake.java

---
+++ Java_Programming_Elements/Java_Programming_Elements/Level_1/handshake.java
@@ -0,0 +1,14 @@
+package immersion;
+
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+//		double d = sc.nextInt();
+//		double b = sc.nextInt();
+//		int q = sc.nextInt();
+		System.out.println((n*(n-1))/2);
+	}
+}
