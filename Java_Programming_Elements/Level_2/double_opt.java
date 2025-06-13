33d674a5fe79eca8f0f57b1352316cd3e99e359e
MukundAgarawal <121181610+MukundAgarawal@users.noreply.github.com>
2025-06-13 09:03:59 +0530

    Create double_opt.java

---
+++ Java_Programming_Elements/Level_2/double_opt.java
@@ -0,0 +1,19 @@
+package immersion;
+
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+//		double d = sc.nextInt();
+//		double h = sc.nextInt();
+		double b = sc.nextInt();
+		double c = sc.nextInt();
+		double o1 = a + b*c;
+		double o2 = a*b + c;
+		double o3 = c + a/b;
+		double o4 = a%b + c;
+		System.out.println("The results of Int Operations are " + o1 + " " + o2 + " " + o3 + " " + o4);
+	}
+}
