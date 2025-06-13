8c3549b9d880a5f67feec0974b6c59ba52cce04b
MukundAgarawal <121181610+MukundAgarawal@users.noreply.github.com>
2025-06-13 08:37:00 +0530

    Create university_discount.java

---
+++ Java_Programming_Elements/Java_Programming_Elements/Level_1/university_discount.java
@@ -0,0 +1,14 @@
+package immersion;
+
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int c = sc.nextInt();
+		int fee = 125000;
+		int dis = 12500;
+//		int b = sc.nextInt();
+		System.out.println("The discount amount is INR " + dis + " and final discounted fee is INR " + (fee-dis));
+	}
+}
