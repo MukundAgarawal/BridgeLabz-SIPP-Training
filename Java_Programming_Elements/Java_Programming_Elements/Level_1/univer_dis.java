13f46148b852f48162fae5e95b4ee4f336756d40
MukundAgarawal <121181610+MukundAgarawal@users.noreply.github.com>
2025-06-13 08:39:21 +0530

    Create univer_dis.java

---
+++ Java_Programming_Elements/Java_Programming_Elements/Level_1/univer_dis.java
@@ -0,0 +1,14 @@
+package immersion;
+
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int c = sc.nextInt();
+		int fee = sc.nextInt();
+		int dis = sc.nextInt();
+//		int b = sc.nextInt();
+		System.out.println("The discount amount is INR " + dis + " and final discounted fee is INR " + (fee-dis));
+	}
+}
